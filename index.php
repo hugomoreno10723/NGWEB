078aed30fff355329dd33803cc9b0945a1025dc9
hugomoreno10723 <hugomoreno10723@gmail.com>
2014-03-12 14:28:26 -0600

    Include bottom_block

--- index.php
+++ index.php
@@ -225,32 +225,8 @@ Realizada por el ministerio Nueva Generación del C. C. Casa de Oración.
 	</div>
 	<div class="clear"></div></div>
 </div>
-<div class="bottom_block">
-	<div class="container_12">
-		<div class="grid_2 prefix_2">
-			<ul>
-				<li><a href="#">Música</a></li>
-				<li><a href="#">Galería</a></li>
-			</ul>
-		</div>
-		<div class="grid_2">
-			<ul>
-				<li><a href="#">Series</a></li>
-				<li><a href="#">Biblicomania</a></li>
-			</ul>
-		</div>
-		<div class="grid_2">
-        	<h4>Fallas técnicas:</h4>
-			comunicacion_ng@hotmail.com
-   		</div>
-		<div class="grid_2">
-			<h4>Contáctanos:</h4>
-			ngjovenes@casadeoracionmexico.com
-		 
-		</div>
-		<div class="clear"></div>
-		</div>
-	</div>
+<!--=======bottom================================-->
+<?php include('./includes/bottom_block.php'); ?>
 <!--==============================footer=================================-->
 
 </div>
--- videos_tardegorra.php
+++ videos_tardegorra.php
@@ -65,32 +65,7 @@ src="http://www.youtube.com/embed/I2mErCSccd4">
 </div>
 
 <!--=======bottom================================-->
-<div class="bottom_block">
-  <div class="container_12">
-    <div class="grid_2 prefix_2">
-			<ul>
-				<li><a href="#">Música</a></li>
-				<li><a href="#">Galería</a></li>
-			</ul>
-		</div>
-		<div class="grid_2">
-			<ul>
-				<li><a href="#">Series</a></li>
-				<li><a href="#">Biblicomania</a></li>
-			</ul>
-		</div>
-		<div class="grid_2">
-        	<h4>Fallas técnicas:</h4>
-			comunicacion_ng@hotmail.com
-   		</div>
-		<div class="grid_2">
-			<h4>Contáctanos:</h4>
-			ngjovenes@casadeoracionmexico.com
-		 
-		</div>
-    <div class="clear"></div>
-    </div>
-  </div>
+<?php include('./includes/bottom_block.php'); ?>
 <!--==============================footer=================================-->
 
 </div>
